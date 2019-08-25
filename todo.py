1adee690c6e14ef72ec7962cd75fff56349c17e0
Akashamba <53367477+Akashamba@users.noreply.github.com>
2019-08-25 22:42:41 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,32 +1,52 @@
 import PySimpleGUI as s
 import back
 
-lst=[]
-lst=back.readall()
+task = back.readtask()
+priority = back.readpriority()
 
-layout=[
-    [s.Text("Enter New Task to be added"),s.InputText("",key='data'),s.Button("ADD")],
-    [s.Slider(orientation='horizontal',key='priority')],
+layout = [
+    [s.Text("Enter New Task to be added"), s.InputText("", key='data'), s.Button("ADD")],
+    [s.Text("Priority Selector")],
+    [s.Slider(orientation='horizontal', key='priority')],
     [s.Text("TO-DO List")],
-    [s.Listbox(lst,size=(50,10),key='box')],
-    [s.Button("CLEAR"),s.Button("CLEAR LIST"),s.Button("EXIT")]
+    [s.Text("Tasks                                                                                       Priority")],
+    [s.Listbox(task, size=(50, 10), key='tbox'), s.Listbox(priority, size=(10, 10), key='pbox')],
+    [s.Button("DELETE"), s.Button("CLEAR LIST"), s.Button("EXIT")]
 ]
 
-w=s.Window("To Do",layout)
+w = s.Window("To Do", layout)
 
 while True:
-    button,values=w.Read()
-    w.FindElement('box').Update(lst)
+    button, values = w.Read()
+    w.FindElement('tbox').Update(task)
+    w.FindElement('pbox').Update(priority)
 
-
-    if button=="ADD":
-        task=values['data']
-        priority=values['priority']
-        back.write(task,priority)
-        lst=back.readall()
+    if button == "ADD":
+        task = values['data']
+        priority = values['priority']
+        back.write(task, priority)
+        task = back.readtask()
+        priority = back.readpriority()
         w.FindElement('data').Update("")
-        w.FindElement('box').Update(lst)
+        w.FindElement('tbox').Update(task)
+        w.FindElement('pbox').Update(priority)
+
+    if button == "CLEAR LIST":
+        back.cleartable()
+        task = back.readtask()
+        priority = back.readpriority()
+        w.FindElement('tbox').Update(task)
+        w.FindElement('pbox').Update(priority)
 
 
-    if button=="EXIT":
+    if button == "DELETE":
+        x = values['tbox'][0]
+        back.delete(x)
+        task = back.readtask()
+        priority = back.readpriority()
+        w.FindElement('tbox').Update(task)
+        w.FindElement('pbox').Update(priority)
+
+
+    if button == "EXIT":
         break
