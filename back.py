7ff842d484f94aede37f2937075548ac5200916d
Akashamba <53367477+Akashamba@users.noreply.github.com>
2019-08-25 22:43:23 +0530

    Update back.py

--- back.py
+++ back.py
@@ -3,21 +3,35 @@ import sqlite3
 
 db = sqlite3.connect('tododb')
 c = db.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS todo(ItemNo INTEGER ,Task TEXT,Priority INTEGER)''')
+c.execute('''CREATE TABLE IF NOT EXISTS todo(Task TEXT,Priority INTEGER)''')
 db.commit()
 
 
 def write(task, priority):
-    c.execute('''INSERT into todo(Task, Priority) VALUES(?,?)''',(task, priority))
+    c.execute('''INSERT into todo (Task, Priority) VALUES(?,?) ''',(task, priority))
     db.commit()
 
 
-def readall():
+def readtask():
     c=db.cursor()
-    c.execute('''select * from todo order by Priority''')
+    c.execute('''select Task from todo order by Priority''')
     data=c.fetchall()
     db.commit()
     return data
 
+def readpriority():
+    c = db.cursor()
+    c.execute('''select Priority from todo order by Priority''')
+    data = c.fetchall()
+    db.commit()
+    return data
 
 
+def cleartable():
+    c.execute('''delete from todo''')
+    db.commit()
+
+
+def delete(x):
+    c.execute('''delete from todo where Task=?''',(x))
+    db.commit()
