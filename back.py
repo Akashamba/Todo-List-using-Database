204e981a4a4f93b0660403318da0f70f67d18d3c
Akashamba <53367477+Akashamba@users.noreply.github.com>
2019-08-27 20:35:19 +0530

    Update back.py

--- back.py
+++ back.py
@@ -1,23 +1,24 @@
 import sqlite3
 
-
 db = sqlite3.connect('tododb')
 c = db.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS todo(Task TEXT,Priority INTEGER)''')
+c.execute('''CREATE TABLE IF NOT EXISTS done(Task TEXT)''')
 db.commit()
 
 
 def write(task, priority):
-    c.execute('''INSERT into todo (Task, Priority) VALUES(?,?) ''',(task, priority))
+    c.execute('''INSERT into todo (Task, Priority) VALUES(?,?) ''', (task, priority))
     db.commit()
 
 
 def readtask():
-    c=db.cursor()
+    c = db.cursor()
     c.execute('''select Task from todo order by Priority''')
-    data=c.fetchall()
+    data = c.fetchall()
     db.commit()
     return data
+
 
 def readpriority():
     c = db.cursor()
@@ -33,5 +34,24 @@ def cleartable():
 
 
 def delete(x):
-    c.execute('''delete from todo where Task=?''',(x))
+    c.execute('''delete from todo where Task=?''', x)
+    db.commit()
+
+
+def readcompleted():
+    c = db.cursor()
+    c.execute('''select Task from done order by Task''')
+    data = c.fetchall()
+    db.commit()
+    return data
+
+
+def deletecompleted(x):
+    c.execute('''delete from done where Task=?''', x)
+    db.commit()
+
+
+def complete(x):
+    c.execute('''INSERT into done (Task) VALUES(?) ''', (x))
+    c.execute('''delete from todo where Task=? ''', (x))
     db.commit()
