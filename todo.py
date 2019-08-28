6062d7c9fdab8bbb282b6c0abfc1f26e669b3ff0
Akashamba <53367477+Akashamba@users.noreply.github.com>
2019-08-28 17:42:37 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -3,14 +3,17 @@ import back
 
 task = back.readtask()
 priority = back.readpriority()
+completed = back.readcompleted()
 
 layout = [
-    [s.Text("Enter New Task to be added"), s.InputText("", key='data'), s.Button("ADD")],
-    [s.Text("Priority Selector")],
-    [s.Slider(orientation='horizontal', key='priority')],
+    [s.Text("Enter New Task to be added"), s.InputText("", key='data')],
+    [s.Text("Priority Selector"), s.Slider(orientation='horizontal', key='priority')],
+    [s.Button("ADD NEW ITEM")],
+    [s.Text("")],
     [s.Text("TO-DO List")],
     [s.Text("Tasks                                                                                       Priority")],
     [s.Listbox(task, size=(50, 10), key='tbox'), s.Listbox(priority, size=(10, 10), key='pbox')],
+    [s.Button("COMPLETED"), s.Button("VIEW COMPLETED")],
     [s.Button("DELETE"), s.Button("CLEAR LIST"), s.Button("EXIT")]
 ]
 
@@ -21,13 +24,16 @@ while True:
     w.FindElement('tbox').Update(task)
     w.FindElement('pbox').Update(priority)
 
-    if button == "ADD":
+    if button == "ADD NEW ITEM":
         task = values['data'].capitalize()
-        task = values['data']
         priority = values['priority']
-        back.write(task, priority)
+
+        if task != "":
+            back.write(task, priority)
+
         task = back.readtask()
         priority = back.readpriority()
+
         w.FindElement('data').Update("")
         w.FindElement('tbox').Update(task)
         w.FindElement('pbox').Update(priority)
@@ -39,7 +45,6 @@ while True:
         w.FindElement('tbox').Update(task)
         w.FindElement('pbox').Update(priority)
 
-
     if button == "DELETE":
         x = values['tbox'][0]
         back.delete(x)
@@ -48,6 +53,43 @@ while True:
         w.FindElement('tbox').Update(task)
         w.FindElement('pbox').Update(priority)
 
+    if button == "COMPLETED":
+        x = values['tbox'][0]
+        back.complete(x)
+        task = back.readtask()
+        priority = back.readpriority()
+        completed = back.readcompleted()
+        w.FindElement('tbox').Update(task)
+        w.FindElement('pbox').Update(priority)
+
+    if button == "VIEW COMPLETED":
+        layoutc = [
+            [s.Text("Completed Tasks")],
+            [s.Listbox(completed, key='comp', size=(50, 10))],
+            [s.Button("REMOVE"), s.Button("REMOVE ALL"), s.Button("CLOSE")]
+        ]
+
+        c = s.Window("Completed", layoutc)
+
+        while True:
+            events, values = c.Read()
+
+            c.FindElement('comp').Update(completed)
+
+            if events == "REMOVE":
+                d = values['comp'][0]
+                back.deletecompleted(d)
+                completed = back.readcompleted()
+                c.FindElement('comp').Update(completed)
+
+            elif events =="REMOVE ALL":
+                back.deleteallcompleted()
+                completed = back.readcompleted()
+                c.FindElement('comp').Update(completed)
+
+            elif events == "CLOSE":
+                c.close()
+                break
 
     if button == "EXIT":
         break
